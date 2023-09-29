36044b4b938dc585848a06058340e5569b9e3c91
Malazarte <mmvmalazarte@tip.edu.ph>
2023-09-29 11:56:48 +0800

    Seatwork(GIT)

--- Malazarte.py
+++ Malazarte.py
@@ -2,5 +2,5 @@ firstname = input ("Enter your firstname: ")
 middilename = input("Enter your middlename: ")
 lastname = input("Enter your lastname: ")
 
-fullname = "(firstname) (middlename) (lastname)"
+fullname = "f{firstname} {middlename} {lastname}"
 print ("Your full name is:", fullname)
